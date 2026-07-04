16447810acef01010ab4f9ed6c7705426d780f27
Swaraj <swarajunde1@gmail.com>
2026-07-04 17:46:10 +0530

    monaco editor p1

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,12 +1,30 @@
-'use client'
+"use client";
 
+import { SidebarInset } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { usePlayground } from "@/modules/playground/hooks/usePlayground";
 import { useParams } from "next/navigation";
 import React from "react";
 
 const MainPlaygroundPage = () => {
   const { id } = useParams<{ id: string }>();
 
-  return <div>Params:{id}</div>;
+  const { playgroundData, templateData, isLoading, error, saveTemplateData } =
+    usePlayground(id);
+
+  console.log("temp",templateData);
+  console.log(playgroundData);
+
+  return (
+    <TooltipProvider>
+      <>
+
+        <SidebarInset>
+           
+        </SidebarInset>
+      </>
+    </TooltipProvider>
+  )
 };
 
 export default MainPlaygroundPage;
---
+++ modules/playground/lib/index.ts
@@ -0,0 +1,39 @@
+import { TemplateFile, TemplateFolder } from "./path-to-json";
+
+export function findFilePath(
+  file: TemplateFile,
+  folder: TemplateFolder,
+  pathSoFar: string[] = [],
+): string | null {
+  for (const item of folder.items) {
+    if ("folderName" in item) {
+      const res = findFilePath(file, item, [...pathSoFar, item.folderName]);
+      if (res) return res;
+    } else {
+      if (
+        item.filename === file.filename &&
+        item.fileExtension === file.fileExtension
+      ) {
+        return [
+          ...pathSoFar,
+          item.filename + (item.fileExtension ? "." + item.fileExtension : ""),
+        ].join("/");
+      }
+    }
+  }
+  return null;
+}
+
+export const generateFileId = (
+  file: TemplateFile,
+  rootFolder: TemplateFolder,
+): string => {
+  const path = findFilePath(file, rootFolder)?.replace(/^\/+/, "") || "";
+
+  const extension = file.fileExtension?.trim();
+  const extensionSuffix = extension ? `.${extension}` : "";
+
+  return path
+    ? `${path}/${file.filename}${extensionSuffix}`
+    : `${file.filename}${extensionSuffix}`;
+};
